5f98f23d662c1d2dd0d36bcf90787a9e01cb9c7f
mjoconnell2 <maryann.oc91@gmail.com>
2016-04-23 18:31:09 -0400

    Create MockData class

---
+++ app/src/main/java/com/darrienglasser/goatfish/MockData.java
@@ -0,0 +1,26 @@
+package com.darrienglasser.goatfish;
+
+/**
+ * Created by maryannoconnell on 4/23/16.
+ */
+public class MockData {
+
+    /* Source: Western & Central Pacific Fisheries Commission
+     * https://www.wcpfc.int/record-fishing-vessel-database
+     */
+
+    // Flag : United States of America
+    Vessel mV1, mV2, mV3, mV4, mV5, mV6, mV7, mV8, mV9, mV10;
+
+    // ADELITA, 1994
+   
+
+
+
+
+
+
+
+
+
+}
