af57525df0a97e46c2fd0a65d2cab83aebd59cf6
Darrien Glasser <darrienglasser@outlook.com>
2016-04-24 00:09:53 -0400

    Push to ready get ready for test data

--- app/src/main/java/com/darrienglasser/goatfish/ResultsActivity.java
+++ app/src/main/java/com/darrienglasser/goatfish/ResultsActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class ResultsActivity extends AppCompatActivity {
 
     ListView mCardView;
@@ -11,7 +13,14 @@ public class ResultsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ArrayList<ResultsMicroGroup> resultsMicroList = new ArrayList<>();
+
         setContentView(R.layout.activity_results);
         mCardView = (ListView) findViewById(R.id.card_view);
+        mCardView.setAdapter(
+                new CardListAdapter(getApplicationContext(), R.id.card_view, resultsMicroList));
+
+        
+
     }
 }
