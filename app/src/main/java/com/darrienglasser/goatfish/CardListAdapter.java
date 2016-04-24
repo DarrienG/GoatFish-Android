a4ff04391a54304a8baf8fd4d30f1cd780d69be4
Darrien Glasser <darrienglasser@outlook.com>
2016-04-24 07:13:57 -0400

    Fix logic errors in list

--- app/src/main/java/com/darrienglasser/goatfish/CardListAdapter.java
+++ app/src/main/java/com/darrienglasser/goatfish/CardListAdapter.java
@@ -2,12 +2,15 @@ package com.darrienglasser.goatfish;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.List;
+import java.util.Objects;
 
 public class CardListAdapter extends ArrayAdapter<ResultsMicroGroup> {
     public CardListAdapter(Context context, int textViewResourceId) {
@@ -20,14 +23,17 @@ public class CardListAdapter extends ArrayAdapter<ResultsMicroGroup> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        Resources res = Resources.getSystem();
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.results_row, parent, false);
+        Resources res = getContext().getResources();
         ResultsMicroGroup group = getItem(position);
-        TextView vesselView = (TextView) convertView.findViewById(R.id.vessel_name);
-        TextView yearView = (TextView) convertView.findViewById(R.id.year);
-        TextView countryView = (TextView) convertView.findViewById(R.id.country);
-        TextView imoView = (TextView) convertView.findViewById(R.id.imo);
-        TextView uviView = (TextView) convertView.findViewById(R.id.uvi);
-        TextView lengthView = (TextView) convertView.findViewById(R.id.length);
+        TextView vesselView = (TextView) v.findViewById(R.id.vessel_name);
+        TextView yearView = (TextView) v.findViewById(R.id.year);
+        TextView countryView = (TextView) v.findViewById(R.id.country);
+        TextView imoView = (TextView) v.findViewById(R.id.imo);
+        TextView uviView = (TextView) v.findViewById(R.id.uvi);
+        TextView lengthView = (TextView) v.findViewById(R.id.length);
         vesselView.setText(String.format(res.getString(R.string.vessel_name), group.getName()));
         yearView.setText(String.format(res.getString(R.string.year), group.getYear()));
         countryView.setText(String.format(res.getString(R.string.country), group.getCountry()));
@@ -39,6 +45,6 @@ public class CardListAdapter extends ArrayAdapter<ResultsMicroGroup> {
                         group.getLength(),
                         group.getUnits()));
 
-        return convertView;
+        return v;
     }
 }
--- app/src/main/java/com/darrienglasser/goatfish/ResultsActivity.java
+++ app/src/main/java/com/darrienglasser/goatfish/ResultsActivity.java
@@ -15,12 +15,12 @@ public class ResultsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ArrayList<ResultsMicroGroup> resultsMicroList = new ArrayList<>();
 
+        ResultsMicroGroup rmg = new ResultsMicroGroup("Darrien", "1995", "IMO124432", "UVIfsf", "293.2", "FT", "USA");
+        resultsMicroList.add(rmg);
         setContentView(R.layout.activity_results);
         mCardView = (ListView) findViewById(R.id.card_view);
         mCardView.setAdapter(
-                new CardListAdapter(getApplicationContext(), R.id.card_view, resultsMicroList));
-
-        
+                new CardListAdapter(this, R.layout.results_row, resultsMicroList));
 
     }
 }
